7e260e040b3893f28bd563064c3d90abf7640432
zhoumingyu <zhoumingyu198710@163.com>
2018-06-04 09:12:59 +0800

    Signed-off-by: zhoumingyu <zhoumingyu198710@163.com>

--- management-system/src/main/java/top/zhoumy/Application.java
+++ management-system/src/main/java/top/zhoumy/Application.java
@@ -1,6 +1,8 @@
 package top.zhoumy;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,8 +12,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("top.zhoumy.*.dao")
 public class Application {
 
+	static Logger logger = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("complete");
+		logger.info("application started");
 	}
 }
